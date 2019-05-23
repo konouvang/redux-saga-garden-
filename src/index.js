88a79d4661f26caf8bb79ae85d53a1211f5d92f7
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-23 11:48:16 -0500

    added post and middleware, code works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+import { takeEvery, put } from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
+
 
 import App from './App';
 
@@ -21,8 +25,34 @@ const plantList = (state = startingPlantArray, action) => {
   }
 };
 
+function* postPlant(action) {
+  console.log(action.payload);
+  try {
+      yield axios.post('/api/plant', action.payload);
+      yield put({
+          type: 'FETCH_PLANTS'
+      });
+      console.log(action.payload);
+  } catch (err) {
+      console.log('error HELP:', err);
+  }
+}
+
+function* watcherSaga() {
+  yield takeEvery('ADD_ELEMENT', postPlant);
+
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList
+  
+  }),
+  applyMiddleware(sagaMiddleware),
 );
+
+sagaMiddleware.run(watcherSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
