27f4e253616574fcf02013541b508140d44e64f2
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-05-23 14:34:44 -0500

    new details component started

---
+++ src/components/Details/Details.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
+class Details extends Component {
+    state = {
+        plant: {
+            name: '',
+            kingdom: '',
+            clade: '',
+            order: '',
+            family: '',
+            subfamily: '',
+            genus: ''
+        }
+    }
+
+    handleNameChange = event => {
+        console.log('event happended')
+        this.setState({
+            newPlant: {
+                ...this.state.newPlant,
+                name: event.target.value,
+            }
+        });
+    }
+
+    componentDidMount() {
+        // this.getElements();
+        this.props.dispatch({
+          type: 'GET_PLANTS'
+        })
+        console.log('This is in NewPlantForm,');
+      }
+
+    addNewPlant = event => {
+        event.preventDefault();
+        this.props.dispatch({ type: 'POST_PLANT', payload: this.state.newPlant })
+        this.setState({
+            newPlant: {
+                id: this.state.newPlant.id + 1,
+                name: '',
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>This is the form</h3>
+                <pre>{JSON.stringify(this.state)}</pre>
+                <form onSubmit={this.addNewPlant}>
+                    <input type='text' value={this.state.newPlant.name} onChange={this.handleNameChange} />
+                    <input type='submit' value='Add New Plant' />
+                </form>
+            </div>
+        );
+    }
+}
+
+
+export default connect(mapStateToProps)(NewPlantForm);
