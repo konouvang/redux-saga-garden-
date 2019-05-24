48a4266c2d6050a828a3a171d85af750e947a13e
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-24 13:50:34 -0500

    commit

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import mapStateToProps from '.../mapStateToProps';
 
 class Details extends Component {
     render(){
